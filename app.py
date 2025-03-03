194b141d49515548f707114f39242522a9d767e8
Seeram Harshavardhan <66600388+SeeramHarsha@users.noreply.github.com>
2025-03-03 12:07:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,5 +120,5 @@ def upload():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Use PORT from Render, default to 10000
+    port = int(os.environ.get("PORT", 8080))  # Use PORT from Render, default to 10000
     app.run(host="0.0.0.0", port=port, debug=True)
